5d0fc772cf4156e3b750e4fbd23a371c2bc598d8
Liu Tianshi <tianshiliu@Tianshis-MacBook-Pro.local>
2016-02-04 16:28:48 -0500

    Trying to work out the input part

--- Homework1/1/Number1.py
+++ Homework1/1/Number1.py
@@ -22,22 +22,39 @@ def inputProcess():
             commands.append (line)
 
     for i in range(0,len(tempCommands),1):
-        #replace all '\n' with ' ', delete comments and combine commands
-        if tempCommands[i].index('\n'):
-
+        #process of comment and return
+        if tempCommands[i].find('\n') or tempCommands[i].find('#'):
+            # the line above is stupid, don't know better way to do it
             commands += ' '
             spaceCount = 1
-            for j in range(1,tempCommands[i].index('\n')-1,1):
-                if tempCommands[i][j] == ' ' and spaceCount ==0 :
+            for j in range(1,max(tempCommands[i].find('\n'), tempCommands[i].find('#')),1):
+                #copy and eliminate extra ' '
+                #However, this part might doesn't matter at all because of split
+                if (tempCommands[i][j] == ' ' or tempCommands[i][j] == ' ')and spaceCount ==0 :
                     commands += ' '
                 else:
                     commands += tempCommands[i][j]
                     spaceCount = 0
+    commands = commands.split()
 
-    for i in range(0,len(commands)):
-        # delete extra ' '
-        pass
-
+    for i in range(0,len(commands)-1,1):
+        #input grammar chack if fails, exit
+        if commands[i] in ['ildc',                       #Fellowed by num
+                          ]:
+            if ~commands[i+1].isnumeric():
+                print("error")
+                exit()
+        elif commands[i] in [
+                           'iadd','isub','imul','idiv'
+                           'pop','dup','swap'
+                           'load','store'                #Related to store
+                          ]:
+        elif commands[i] in [
+                           'jz','jnz',                   #Fellowed by label
+                          ]:
+            errorflag = 0
+            if ~commands[i+1][0]
+        elif 
 
 inputProcess()
 print(commands)
